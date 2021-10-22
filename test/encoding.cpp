3d67c8e508f18e9b68334e7c8f347c68094cd57d
Dr-Fabulous <71410010+Dr-Fabulous@users.noreply.github.com>
2021-10-22 18:51:49 +0100

    added tests for UTF-8, 16 & 32

--- test/encoding.cpp
+++ test/encoding.cpp
@@ -6,13 +6,15 @@
 #include <tuple>
 #include <array>
 #include <string_view>
-#include <iostream>
 #include "fb/comby/encoding.hpp"
+#include "fb/comby/ascii.hpp"
 #include "fb/comby/utf8.hpp"
-#include "fb/comby/parser.hpp"
+#include "fb/comby/utf16.hpp"
+#include "fb/comby/utf32.hpp"
+#include "fb/comby/locale.hpp"
 
 using namespace std::literals;
-using namespace fb::comby;
+using namespace fb::comby::encoding;
 
 bool compare(auto const& a, auto const& b) noexcept {
 	for (auto i = std::size_t{}, s = std::min(std::ranges::size(a), std::ranges::size(b)); i < s; ++i) {
@@ -26,74 +28,121 @@ bool compare(auto const& a, auto const& b) noexcept {
 	return true;
 }
 
-template <typename R, typename T>
-concept contiguous_range_of = std::ranges::contiguous_range<R>
-			   && std::is_same_v<std::ranges::range_value_t<R>, T>;
+void test_utf8() noexcept {
+	auto units = std::array<unit_t<utf8>, utf8::max_units>{};
+	auto codes = std::array<code_t<utf8>, utf8::max_codes>{};
+	auto state = state_t<utf8>{};
 
-template <encoding::encoding E>
-void test_encode_decode(contiguous_range_of<encoding::unit_t<E>> auto const& test_units,
-			contiguous_range_of<encoding::code_t<E>> auto const& test_codes) noexcept
-{
-	auto units = std::array<encoding::unit_t<E>, E::max_units>{};
-	auto codes = std::array<encoding::code_t<E>, E::max_codes>{};
-	auto state = encoding::state_t<E>{};
+	auto const encode_decode = [&](std::u8string_view test_units, std::u32string_view test_codes) {
+		assert(utf8::encode(state, test_codes, units));
+		assert(compare(test_units, units));
+		assert(utf8::decode(state, test_units, codes));
+		assert(compare(test_codes, codes));
+	};
 
-	assert(E::decode(state, test_units, codes));
-	assert(compare(test_codes, codes));
-	assert(E::encode(state, test_codes, units));
-	assert(compare(test_units, units));
+	encode_decode(u8"A"sv, U"A"sv);
+	encode_decode(u8"\u0400"sv, U"\u0400"sv);
+	encode_decode(u8"\uFFFD"sv, U"\uFFFD"sv);
+	encode_decode(u8"\U0010AAAA"sv, U"\U0010AAAA"sv);
+
+	auto empty_units = std::array<unit_t<utf8>, 0>{};
+	auto wrong_units = std::array<char8_t, 2>{0b11000011u, 0u};
+	auto empty_codes = std::array<code_t<utf8>, 0>{};
+	auto invalid_codes = std::array<char32_t, 1>{0x111111u};
+
+	assert(utf8::encode(state, empty_codes, units).code == result_code::OK);
+	assert(utf8::encode(state, empty_codes, empty_units).code == result_code::OK);
+	assert(utf8::encode(state, empty_codes, units).code == result_code::OK);
+
+	assert(utf8::decode(state, empty_units, codes).code == result_code::OK);
+	assert(utf8::decode(state, empty_units, empty_codes).code == result_code::OK);
+	assert(utf8::decode(state, wrong_units, codes).code == result_code::INVALID_ENCODING);
+
+	assert(utf8::encode(state, std::array<char32_t, 1>{0xD8FF}, units).code == result_code::INVALID_CODE_POINT);
+	assert(utf8::encode(state, std::array<char32_t, 1>{0xDCFF}, units).code == result_code::INVALID_CODE_POINT);
+	assert(utf8::encode(state, std::array<char32_t, 1>{0x11FFFF}, units).code == result_code::INVALID_CODE_POINT);
+
+	assert(utf8::decode(state, std::array<char8_t, 3>{0b11101101u, 0b10100000u, 0b10000001u}, codes).code == result_code::INVALID_CODE_POINT);
+	assert(utf8::decode(state, std::array<char8_t, 3>{0b11101101u, 0b10110000u, 0b10000001u}, codes).code == result_code::INVALID_CODE_POINT);
 }
 
-template <encoding::encoding E>
-void test_edge_cases(contiguous_range_of<encoding::unit_t<E>> auto const& invalid_units,
-		     contiguous_range_of<encoding::code_t<E>> auto const& invalid_codes,
-		     contiguous_range_of<encoding::unit_t<E>> auto const& wrong_units) noexcept
-{
-	auto units = std::array<encoding::unit_t<E>, E::max_units>{};
-	auto codes = std::array<encoding::code_t<E>, E::max_codes>{};
-	auto empty_units = std::array<encoding::unit_t<E>, 0>{};
-	auto empty_codes = std::array<encoding::code_t<E>, 0>{};
-	auto state = encoding::state_t<E>{};
+void test_utf16() noexcept {
+	auto units = std::array<unit_t<utf16>, utf16::max_units>{};
+	auto codes = std::array<code_t<utf16>, utf16::max_codes>{};
+	auto state = state_t<utf16>{};
 
-	assert(E::decode(state, empty_units, codes).code == encoding::result_code::OK); state = {};
-	assert(E::decode(state, units, empty_codes).code == encoding::result_code::NOT_ENOUGH_STORAGE); state = {};
-	assert(E::decode(state, empty_units, empty_codes).code == encoding::result_code::OK); state = {};
-	assert(E::decode(state, invalid_units, codes).code == encoding::result_code::INVALID_CODE_POINT); state = {};
-	assert(E::decode(state, wrong_units, codes).code == encoding::result_code::INVALID_ENCODING); state = {};
+	auto const encode_decode = [&](std::u16string_view test_units, std::u32string_view test_codes) {
+		assert(utf16::encode(state, test_codes, units));
+		assert(compare(test_units, units));
+		assert(utf16::decode(state, test_units, codes));
+		assert(compare(test_codes, codes));
+	};
 
-	assert(E::encode(state, empty_codes, units).code == encoding::result_code::OK); state = {};
-	assert(E::encode(state, codes, empty_units).code == encoding::result_code::NOT_ENOUGH_STORAGE); state = {};
-	assert(E::encode(state, empty_codes, empty_units).code == encoding::result_code::OK); state = {};
-	assert(E::encode(state, invalid_codes, units).code == encoding::result_code::INVALID_CODE_POINT);
+	encode_decode(u"A"sv, U"A"sv);
+	encode_decode(u"\u0400"sv, U"\u0400"sv);
+	encode_decode(u"\uFFFD"sv, U"\uFFFD"sv);
+	encode_decode(u"\U0010AAAA"sv, U"\U0010AAAA"sv);
+
+	auto empty_units = std::array<unit_t<utf16>, 0>{};
+	auto wrong_units = std::array<char16_t, 2>{0xD811u, 0x65u};
+	auto empty_codes = std::array<code_t<utf16>, 0>{};
+	auto invalid_codes = std::array<char32_t, 1>{0x111111u};
+
+	assert(utf16::encode(state, empty_codes, units).code == result_code::OK);
+	assert(utf16::encode(state, empty_codes, empty_units).code == result_code::OK);
+	assert(utf16::encode(state, empty_codes, units).code == result_code::OK);
+	assert(utf16::encode(state, invalid_codes, units).code == result_code::INVALID_CODE_POINT);
+
+	assert(utf16::decode(state, empty_units, codes).code == result_code::OK);
+	assert(utf16::decode(state, empty_units, empty_codes).code == result_code::OK);
+	assert(utf16::decode(state, wrong_units, codes).code == result_code::INVALID_ENCODING);
+
+	assert(utf16::encode(state, std::array<char32_t, 1>{0xD8FF}, units).code == result_code::INVALID_CODE_POINT);
+	assert(utf16::encode(state, std::array<char32_t, 1>{0xDCFF}, units).code == result_code::INVALID_CODE_POINT);
+	assert(utf16::encode(state, std::array<char32_t, 1>{0x11FFFF}, units).code == result_code::INVALID_CODE_POINT);
+}
+
+void test_utf32() noexcept {
+	auto units = std::array<unit_t<utf32>, utf32::max_units>{};
+	auto codes = std::array<code_t<utf32>, utf32::max_codes>{};
+	auto state = state_t<utf32>{};
+
+	auto const encode_decode = [&](std::u32string_view test_units, std::u32string_view test_codes) {
+		assert(utf32::encode(state, test_codes, units));
+		assert(compare(test_units, units));
+		assert(utf32::decode(state, test_units, codes));
+		assert(compare(test_codes, codes));
+	};
+
+	encode_decode(U"A"sv, U"A"sv);
+	encode_decode(U"\u0400"sv, U"\u0400"sv);
+	encode_decode(U"\uFFFD"sv, U"\uFFFD"sv);
+	encode_decode(U"\U0010AAAA"sv, U"\U0010AAAA"sv);
+
+	auto empty_units = std::array<unit_t<utf32>, 0>{};
+	auto wrong_units = std::array<char32_t, 1>{0xD811u};
+	auto empty_codes = std::array<code_t<utf32>, 0>{};
+	auto invalid_codes = std::array<char32_t, 1>{0x111111u};
+
+	assert(utf32::encode(state, empty_codes, units).code == result_code::OK);
+	assert(utf32::encode(state, empty_codes, empty_units).code == result_code::OK);
+	assert(utf32::encode(state, empty_codes, units).code == result_code::OK);
+	assert(utf32::encode(state, invalid_codes, units).code == result_code::INVALID_CODE_POINT);
+
+	assert(utf32::decode(state, empty_units, codes).code == result_code::OK);
+	assert(utf32::decode(state, empty_units, empty_codes).code == result_code::OK);
+	assert(utf32::decode(state, wrong_units, codes).code == result_code::INVALID_ENCODING);
+
+	assert(utf32::encode(state, std::array<char32_t, 1>{0xD8FF}, units).code == result_code::INVALID_CODE_POINT);
+	assert(utf32::encode(state, std::array<char32_t, 1>{0xDCFF}, units).code == result_code::INVALID_CODE_POINT);
+	assert(utf32::encode(state, std::array<char32_t, 1>{0x11FFFF}, units).code == result_code::INVALID_CODE_POINT);
+
 }
 
 int main(int argc, char const* args[]) {
-	// encode/decode characters that range 1-4 bytes
-	test_encode_decode<encoding::utf8>(u8"A"sv, U"A"sv);
-	test_encode_decode<encoding::utf8>(u8"\u0400"sv, U"\u0400"sv);
-	test_encode_decode<encoding::utf8>(u8"\uFFFD"sv, U"\uFFFD"sv);
-	test_encode_decode<encoding::utf8>(u8"\U0010AAAA"sv, U"\U0010AAAA"sv);
-
-	// units and codes that go beyond 0x10FFFF and an invalid unit sequence
-	test_edge_cases<encoding::utf8>(std::array<char8_t, 4>{0b11110111, 0b10111111, 0b10111111, 0b10111111},
-					std::array<char32_t, 1>{0x111111},
-					std::array<char8_t, 2>{0b11000011, 0});
-
-	// surrogate values
-	{
-		auto surrogate_units_1 = std::array<char8_t, 3>{0b11101101, 0b10100000, 0b10000001};
-		auto surrogate_units_2 = std::array<char8_t, 3>{0b11101101, 0b10110000, 0b10000001};
-		auto surrogate_codes_1 = std::array<char32_t, 1>{0xD801};
-		auto surrogate_codes_2 = std::array<char32_t, 1>{0xDC01};
-		auto units = std::array<encoding::unit_t<encoding::utf8>, encoding::utf8::max_units>{};
-		auto codes = std::array<encoding::code_t<encoding::utf8>, encoding::utf8::max_codes>{};
-		auto state = encoding::utf8::state_type{};
-
-		assert(encoding::utf8::decode(state, surrogate_units_1, codes).code == encoding::result_code::INVALID_CODE_POINT);
-		assert(encoding::utf8::decode(state, surrogate_units_2, codes).code == encoding::result_code::INVALID_CODE_POINT);
-		assert(encoding::utf8::encode(state, surrogate_codes_1, units).code == encoding::result_code::INVALID_CODE_POINT);
-		assert(encoding::utf8::encode(state, surrogate_codes_2, units).code == encoding::result_code::INVALID_CODE_POINT);
-	}
+	test_utf8();
+	test_utf16();
+	test_utf32();
 
 	return 0;
 }
