2dc1b4b4114b1661c873a28a1e04e960e02bb34f
Dr-Fabulous <71410010+Dr-Fabulous@users.noreply.github.com>
2021-10-22 18:49:46 +0100

    added version of UTF-8 encoding that can take chars as units

--- comby/inc/fb/comby/utf8.hpp
+++ comby/inc/fb/comby/utf8.hpp
@@ -3,12 +3,14 @@
 #include <cstddef>
 #include <ranges>
 #include <algorithm>
+#include <span>
 #include "fb/comby/encoding.hpp"
 
 namespace fb::comby::encoding {
-	class utf8 {
+	template <typename U>
+	class base_utf8 {
 	public:
-		using unit_type = char8_t;
+		using unit_type = U;
 		using code_type = char32_t;
 		struct state_type {};
 
@@ -16,13 +18,7 @@ namespace fb::comby::encoding {
 		static constexpr std::size_t max_codes = 1;
 
 	private:
-		static constexpr bool valid_cp(code_type cp) noexcept {
-			return !(cp > 0xD800 && cp < 0xDBFF) // surrogate pair ranges
-			    && !(cp > 0xDC00 && cp < 0xDFFF)
-			    && cp <= 0x10FFFF; // maximum cp
-		}
-
-		enum unit_class : unit_t<utf8> {
+		enum unit_class : unit_t<base_utf8> {
 			INVALID,
 			LB_1, // ASCII
 			TB, // trailing byte
@@ -36,7 +32,7 @@ namespace fb::comby::encoding {
 			auto arr = std::array<unit_class, 256>{};
 
 			for (auto i = std::size_t{}; i < std::ranges::size(arr); ++i) {
-				auto j = static_cast<unit_t<utf8>>(i);
+				auto j = static_cast<unit_t<base_utf8>>(i);
 
 				if (j < 0x80) {
 					arr[i] = unit_class::LB_1;
@@ -56,7 +52,7 @@ namespace fb::comby::encoding {
 			return arr;
 		}();
 
-		enum decode_state : unit_t<utf8> {
+		enum decode_state : unit_t<base_utf8> {
 			DONE,
 			ERROR,
 			_3,
@@ -74,90 +70,71 @@ namespace fb::comby::encoding {
 		};
 
 	public:
-		constexpr utf8() = default;
-		constexpr utf8(utf8 const&) = default;
-		constexpr utf8(utf8&&) = default;
+		constexpr base_utf8() = default;
+		constexpr base_utf8(base_utf8 const&) = default;
+		constexpr base_utf8(base_utf8&&) = default;
 
-		constexpr utf8& operator=(utf8 const&) = default;
-		constexpr utf8& operator=(utf8&&) = default;
+		constexpr base_utf8& operator=(base_utf8 const&) = default;
+		constexpr base_utf8& operator=(base_utf8&&) = default;
 
-		static constexpr encode_result<utf8> encode(state_type&,
-							    std::span<code_type const> src,
-							    std::span<unit_type> dst) noexcept
+		static constexpr encode_result<base_utf8> encode(state_type&,
+								 std::span<code_type const> src,
+								 std::span<unit_type> dst) noexcept
 		{
 			if (src.empty()) {
-				return {result_code::OK, src, dst};
+				return {result_code::OK};
 			}
 
 			auto const cp = src[0];
 
-			if (!valid_cp(cp)) {
-				return {result_code::INVALID_CODE_POINT, src, dst};
+			if (!is_unicode_scalar(cp)) {
+				return {result_code::INVALID_CODE_POINT, src.subspan(0, 1)};
 			} else if (cp < 0x80) {
-				if (dst.empty()) {
-					return {result_code::NOT_ENOUGH_STORAGE, src, dst};
-				}
-
 				dst[0] = static_cast<unit_type>(cp);
 
-				return {result_code::OK, src.subspan(1), dst.subspan(1)};
+				return {result_code::OK, src.subspan(0, 1), dst.subspan(0, 1)};
 			} else if (cp < 0x800) {
-				if (dst.size() < 2) {
-					return {result_code::NOT_ENOUGH_STORAGE, src, dst};
-				}
-
 				dst[0] = 0b11000000 | (static_cast<unit_type>(cp >> 6) & 0b00011111);
 				dst[1] = 0b10000000 | (static_cast<unit_type>(cp) & 0b00111111);
 
-				return {result_code::OK, src.subspan(1), dst.subspan(2)};
+				return {result_code::OK, src.subspan(0, 1), dst.subspan(0, 2)};
 			} else if (cp < 0x10000) {
-				if (dst.size() < 3) {
-					return {result_code::NOT_ENOUGH_STORAGE, src, dst};
-				}
-
 				dst[0] = 0b11100000 | (static_cast<unit_type>(cp >> 12) & 0b00001111);
 				dst[1] = 0b10000000 | (static_cast<unit_type>(cp >> 6) & 0b00111111);
 				dst[2] = 0b10000000 | (static_cast<unit_type>(cp) & 0b00111111);
 
-				return {result_code::OK, src.subspan(1), dst.subspan(3)};
+				return {result_code::OK, src.subspan(0, 1), dst.subspan(0, 3)};
 			} else if (cp <= 0x10FFFF) {
-				if (dst.size() < 4) {
-					return {result_code::NOT_ENOUGH_STORAGE, src, dst};
-				}
-
 				dst[0] = 0b11110000 | (static_cast<unit_type>(cp >> 18) & 0b00000111);
 				dst[1] = 0b10000000 | (static_cast<unit_type>(cp >> 12) & 0b00111111);
 				dst[2] = 0b10000000 | (static_cast<unit_type>(cp >> 6) & 0b00111111);
 				dst[3] = 0b10000000 | (static_cast<unit_type>(cp) & 0b00111111);
 
-				return {result_code::OK, src.subspan(1), dst.subspan(4)};
+				return {result_code::OK, src.subspan(0, 1), dst.subspan(0, 4)};
 			} else {
-				return {result_code::INVALID_CODE_POINT, src, dst};
+				return {result_code::INVALID_CODE_POINT, src.subspan(0, 1)};
 			}
 		}
 
-		static constexpr decode_result<utf8> decode(state_type&,
-							    std::span<unit_type const> src,
-							    std::span<code_type> dst) noexcept
+		static constexpr decode_result<base_utf8> decode(state_type&,
+								 std::span<unit_type const> src,
+								 std::span<code_type> dst) noexcept
 		{
 			if (src.empty()) {
-				return {result_code::OK, src, dst};
-			} else if (dst.empty()) {
-				return {result_code::NOT_ENOUGH_STORAGE, src, dst};
+				return {result_code::OK};
 			}
 
 			auto s = decode_state::DONE;
 			auto src_pos = std::ranges::begin(src);
 			auto src_end = std::ranges::end(src);
-			auto dst_pos = std::ranges::begin(dst);
 
 			do {
 				auto const c = unit_to_class[*src_pos];
 
 				if (s == decode_state::DONE) {
-					*dst_pos = (0xFF >> c) & *src_pos;
+					dst[0] = (0xFF >> c) & *src_pos;
 				} else {
-					*dst_pos = (*dst_pos << 6) | static_cast<code_type>(*src_pos & 0x3F);
+					dst[0] = (dst[0] << 6) | static_cast<code_type>(*src_pos & 0x3F);
 				}
 
 				s = class_to_state[static_cast<unit_type>(unit_class::_MAX)
@@ -170,16 +147,19 @@ namespace fb::comby::encoding {
 			     && s != decode_state::ERROR);
 
 			if (s == decode_state::DONE) {
-				if (valid_cp(dst[0])) {
-					return {result_code::OK, decltype(src){src_pos, src_end}, dst.subspan(1)};
+				if (is_unicode_scalar(dst[0])) {
+					return {result_code::OK, src.subspan(0, src_end - src_pos), dst.subspan(0, 1)};
 				} else {
-					return {result_code::INVALID_CODE_POINT, decltype(src){src_pos, src_end}, dst};
+					return {result_code::INVALID_CODE_POINT, src.subspan(0, src_end - src_pos), dst.subspan(0, 1)};
 				}
 			} else {
-				return {result_code::INVALID_ENCODING, decltype(src){src_pos, src_end}, dst};
+				return {result_code::INVALID_ENCODING, src.subspan(0, src_end - src_pos)};
 			}
 		}
 	};
+
+	using utf8 = base_utf8<char8_t>;
+	using utf8_compat = base_utf8<char>;
 }
 
 #endif
